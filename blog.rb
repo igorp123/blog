748d31317d1665d562e1fbe28dc49a42b055b759
igor <ig.plotnikov@gmail.com>
2017-01-14 13:10:50 +0300

    add init_db & before

--- blog.rb
+++ blog.rb
@@ -1,6 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+
+def init_db
+	@db=SQLite3::Database.new 'blog.db'
+	@db.results_as_hash=true
+end
+
+before do
+	init_db
+end
+
 
 get '/' do
 	erb "Hello!"			
